1abbebc59c8f3752a23d8d28a6aa03524d5d505d
Anthony-Nacco <anthonyjnacco@gmail.com>
2026-05-13 10:57:04 -0400

    Refactor main function for improved output
    Updated main function to include path variable and modified report format.

--- main.py
+++ main.py
@@ -6,19 +6,25 @@ def get_book_text(filepath):
     
 
 def main():
+    path = "books/frankenstein.txt"
     text = get_book_text("books/frankenstein.txt")
+    
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path}...")
+    print("----------- Word Count ----------")
     num_words = count_words(text)
     print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    
 
     char_counts = count_characters(text)
     sorted_chars = sort_chars(char_counts)
 
-    print("--- Begin report of books/frankenstein.txt ---")
     for entry in sorted_chars:
         if not entry["char"].isalpha():
             continue
-        print(f"The '{entry['char']}' character was found {entry['num']} times")
-    print("--- End Report ---")    
+        print(f"{entry['char']}: {entry['num']}")
+    print("============= END ===============")    
 
 
-main()
+main(
