fbd4b2ef86daea80d5a24f5d7b3f1b4e82c93c0f
Anthony-Nacco <anthonyjnacco@gmail.com>
2026-05-13 10:43:09 -0400

    Updated to add report

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words, count_characters
+from stats import count_words, count_characters, sort_chars
 
 def get_book_text(filepath):
     with open(filepath) as file:
@@ -9,7 +9,16 @@ def main():
     text = get_book_text("books/frankenstein.txt")
     num_words = count_words(text)
     print(f"Found {num_words} total words")
+
     char_counts = count_characters(text)
-    print(char_counts)
+    sorted_chars = sort_chars(char_counts)
+
+    print("--- Begin report of books/frankenstein.txt ---")
+    for entry in sorted_chars:
+        if not entry["char"].isalpha():
+            continue
+        print(f"The '{entry['char']}' character was found {entry['num']} times")
+    print("--- End Report ---")    
+
 
 main()
